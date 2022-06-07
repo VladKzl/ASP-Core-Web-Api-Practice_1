988dd4f0ac1435ac0390c9ca84378ea98a6ab899
Vlad <vladkozlov2012@mail.ru>
2022-06-08 00:43:17 +0300

    Raplace ForMember() to ForCtorParam()

--- MappingProfile.cs
+++ MappingProfile.cs
@@ -1,6 +1,16 @@
-﻿namespace CompanyEmployees
+﻿using AutoMapper;
+using Entities.Models;
+using Shared.DataTransferObjects;
+
+namespace CompanyEmployees
 {
-    public class Class
+    public class MappingProfile : Profile
     {
+        public MappingProfile()
+        {
+            CreateMap<Company, CompanyDto>()
+            .ForCtorParam("FullAddress",
+            opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+        }
     }
 }
