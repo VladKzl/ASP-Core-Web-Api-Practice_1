b6a20b3279c549c71b5d9e45ff3cafd37fdd4b20
Vlad <vladkozlov2012@mail.ru>
2022-06-19 16:01:16 +0300

    Add (GET) CompanyController.GetCompanyCollection() and additional code

--- Presentation/Controllers/CompaniesController.cs
+++ Presentation/Controllers/CompaniesController.cs
@@ -31,5 +31,11 @@ namespace CompanyEmployees.Presentation.Controllers
             return CreatedAtRoute("CompanyById", new { id = createdCompany.Id },
             createdCompany);
         }
+        [HttpGet("collection/({ids})", Name = "CompanyCollection")]
+        public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+        {
+            var companies = _service.CompanyService.GetByIds(ids, trackChanges: false);
+            return Ok(companies);
+        }
     }
 }
